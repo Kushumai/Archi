2ba8ef2e886385532e06bed525ce1a06e5613e59
Kushumai <clemaildeclem@gmail.com>
2024-11-05 00:44:51 +0100

    fix docker build

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  controllers: [AuthController],
+  imports: [TypeOrmModule.forFeature([User])], // Importe l'entité User
   providers: [AuthService],
+  controllers: [AuthController],
+  exports: [TypeOrmModule],
 })
 export class AuthModule {}
