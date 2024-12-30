4dd24e77badb1513f995b799686cb7aba73ae9c3
Kushumai <clemaildeclem@gmail.com>
2024-12-30 17:02:32 +0100

    .

---
+++ frontend/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: ['@babel/preset-env', '@babel/preset-react'], // ou autres presets que vous utilisez
+    plugins: [
+      '@babel/plugin-proposal-private-property-in-object',
+      // autres plugins nécessaires
+    ],
+  };
