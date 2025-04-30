91f7719da39aacf389b2fdb0eff31d5661a91c91
clement coadou <clemaildeclem@gmail.com>
2025-04-30 17:22:06 +0200

    feat(user-service): add user entity

---
+++ user-service/src/data-source.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entities/user.entity';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DATABASE_URL } = process.env;
+if (!DATABASE_URL) {
+  throw new Error('Missing DATABASE_URL in environment');
+}
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: DATABASE_URL,
+  synchronize: true,
+  logging: false,
+  entities: [],       // on ajoutera vos entités ultérieurement
+  migrations: [],
+  subscribers: [],
+});
