eea03902c043001c8b90cb13301a6d5997443ff1
clement coadou <clemaildeclem@gmail.com>
2025-06-12 19:09:03 +0200

    refactor(auth-module): reorganize imports and enhance JWT module configuration

--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
-import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
   imports: [
+    ConfigModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
-      }),
       inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${config.get<number>('ACCESS_TOKEN_TTL') || 3600}s`,
+        },
+      }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
-export class AuthModule { }
+export class AuthModule {}
